4e9c8907f2d7c4f3a69c25a454cacf5dcb5b735a
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-03-21 13:38:39 +0900

    https経由で叩けるように修正

--- template.rb
+++ template.rb
@@ -1,5 +1,5 @@
 def apply_template!
-  setup_source_paths
+  add_template_repository_to_source_path
 
   template 'Gemfile', force: true
 
@@ -40,8 +40,30 @@ def git_commit
   git commit: %Q{ -m 'Initial commit' }
 end
 
-def setup_source_paths
-  source_paths.unshift File.dirname __FILE__
+require "fileutils"
+require "shellwords"
+
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("rails-template-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/shota-yamashita/rails-template.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{rails-template/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
 end
 
 apply_template!
