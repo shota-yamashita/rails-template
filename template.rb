352268ebcd063251f86056fb29ad79c8839320bf
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-03-24 04:56:46 +0900

    refactoring

--- template.rb
+++ template.rb
@@ -45,23 +45,22 @@ def git_commit
   git commit: %Q{ -m 'Initial commit' }
 end
 
-require 'fileutils'
-require 'shellwords'
-
 # Add this template directory to source_paths so that Thor actions like
 # copy_file and template resolve against our source files. If this file was
 # invoked remotely via HTTP, that means the files are not present locally.
 # In that case, use `git clone` to download them to a local temporary dir.
 def add_template_repository_to_source_path
   if __FILE__ =~ %r{\Ahttps?://}
-    require "tmpdir"
-    source_paths.unshift(tempdir = Dir.mktmpdir("rails-template-"))
+    require 'tmpdir'
+    require 'fileutils'
+    require 'shellwords'
+    source_paths.unshift(tempdir = Dir.mktmpdir('rails-template-'))
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
-      "--quiet",
-      "https://github.com/shota-yamashita/rails-template.git",
+      '--quiet',
+      'https://github.com/shota-yamashita/rails-template.git',
       tempdir
-    ].map(&:shellescape).join(" ")
+    ].map(&:shellescape).join(' ')
 
     if (branch = __FILE__[%r{rails-template/(.+)/template.rb}, 1])
       Dir.chdir(tempdir) { git checkout: branch }
